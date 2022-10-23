409dd9929c636cb30d8af94b3ed67a8a1984bbdc
nargiz92 <ada.tag.92@mail.ru>
2022-10-23 17:57:28 +0300

    commit Hw3 to do.

--- src/s2-homeworks/hw03/tests/pureAddUser.test.tsx
+++ src/s2-homeworks/hw03/tests/pureAddUser.test.tsx
@@ -42,25 +42,3 @@ test('name 3', () => {
     expect(error).toBe('Ошибка! Введите имя!')
     expect(added).toBe(false)
 })
-
-// test('name 1', () => {
-//     name = '1'
-//     pureAddUser(name, setError, setName, addUserCallback)
-//     expect(name).toBe('')
-//     expect(error).toBe('')
-//     expect(added).toBe(true)
-// })
-// test('name 2', () => {
-//     name = ''
-//     pureAddUser(name, setError, setName, addUserCallback)
-//     expect(name).toBe('')
-//     expect(error).toBe('Ошибка! Введите имя!')
-//     expect(added).toBe(false)
-// })
-// test('name 3', () => {
-//     name = ''
-//     pureAddUser(name, setError, setName, addUserCallback)
-//     expect(name).toBe(' ')
-//     expect(error).toBe('Ошибка! Введите имя!')
-//     expect(added).toBe(false)
-// })
