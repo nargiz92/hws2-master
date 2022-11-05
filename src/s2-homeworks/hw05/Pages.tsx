258130674c6fe0a151b82206667c17e5b14652d4
nargiz92 <ada.tag.92@mail.ru>
2022-11-05 10:37:11 +0300

    commit routers for Error

--- src/s2-homeworks/hw05/Pages.tsx
+++ src/s2-homeworks/hw05/Pages.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Routes, Route, Navigate } from 'react-router-dom'
+import {Routes, Route, Navigate} from 'react-router-dom'
 import Error404 from './pages/Error404'
 import PreJunior from './pages/PreJunior'
 import Junior from './pages/Junior'
@@ -21,22 +21,20 @@ function Pages() {
                 {/*<Route path={'/'}  element={PATH.PRE_JUNIOR}/>*/}
                 <Route path={'/'} element={<Navigate to={PATH.PRE_JUNIOR}/>}/>
 
-                <Route path={PATH.PRE_JUNIOR} element={<PreJunior/>}/>
                 {/*роуты для /pre-junior, /junior, /junior-plus*/}
-                <Route path={'/'} element={<Navigate to={PATH.JUNIOR_PLUS}/>}/>
-
-                <Route path={PATH.JUNIOR_PLUS} element={<JuniorPlus/>}/>
-
-                <Route path={'/'} element={<Navigate to={PATH.JUNIOR}/>}/>
+                <Route path={PATH.PRE_JUNIOR} element={<PreJunior/>}/>
 
                 <Route path={PATH.JUNIOR} element={<Junior/>}/>
 
+                <Route path={PATH.JUNIOR_PLUS} element={<JuniorPlus/>}/>
+
                 {/*роут для несуществующей страницы должен отрисовать <Error404 />*/}
-                <Route element={<Error404/>}/>
+                <Route path='*' element={<Error404/>}/>
             </Routes>
         </div>
     )
 }
+
 // Для браузерных проектов есть BrowserRouter и HashRouter компоненты.
 //     BrowserRouter — следует использовать когда вы обрабатываете на сервере динамические запросы,
 //     а HashRouter используйте когда у вас статический веб сайт.Оба из них создадут специальный объект истории. Вы должны использовать BrowserRouter, если у вас есть сервер, который отвечает на запросы,
