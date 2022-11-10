dfff9e1d40c9820f2416924f369b8c615b363020
nargiz92 <ada.tag.92@mail.ru>
2022-11-10 15:18:36 +0300

    commit HW8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,17 +4,43 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
 
-            return state // need to fix
+            if (action.payload === 'up') {
+              return  [...state].sort((u1, u2) => u1.name.localeCompare(u2.name))
+            }
+            if (action.payload === 'down') {
+               return [...state].sort((u1, u2) => u2.name.localeCompare(u1.name))
+            } else
+                return state // need to fix
         }
         case 'check': {
-
-            return state // need to fix
+            action.payload === 18
+            return state.filter(el => el.age >= 18) // need to fix
         }
         default:
             return state
     }
 }
+
+//
+// export const sortUpAC=('up':string,)=>{
+//     return{
+//         type:'sort',
+//         payloud:'up',''
+//
+//     }as const
+// }
+//
+//
+// export const check18AC=(payload:number)=>{
+//     return{
+//         type:'check',
+//         payloud:18
+//
+//     }as const
+// }
+//
+
