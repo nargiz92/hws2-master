e3ef0eac4022ed429fe6c9763bed14606c7115a0
nargiz92 <ada.tag.92@mail.ru>
2022-12-24 12:37:01 +0300

    commit HW8

--- src/s2-homeworks/hw10/bll/loadingReducer.ts
+++ src/s2-homeworks/hw10/bll/loadingReducer.ts
@@ -1,10 +1,17 @@
-const initState = {
+export const initState = {
     isLoading: false,
 }
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
+export type InitialStateType= {
+    isLoading: boolean
+}
+
+export const loadingReducer = (state: InitialStateType = initState, action: LoadingActionType):InitialStateType => { // fix any
     switch (action.type) {
         // пишет студент  // need to fix
+        case 'CHANGE_LOADING':{
+            return {...state, isLoading: action.isLoading}
+        }
 
         default:
             return state
